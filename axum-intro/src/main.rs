1351e06de57201eac649917970334c53c75a5531
Alex Rybalko <aagern@gmail.com>
2025-12-16 21:00:46 +0300

    hello handler and test added

--- axum-intro/src/main.rs
+++ axum-intro/src/main.rs
@@ -1,25 +1,6 @@
-// #![allow(unused)]
-// use axum::response::Html;
-// use axum::{Router, routing::get};
-// use std::net::SocketAddr;
-// use tokio::net::TcpListener;
-
-// #[tokio::main]
-// async fn main() {
-//     let routes_hello = Router::new().route(
-//         "/hello",
-//         get(|| async { Html("Hello <strong>World!</strong>") }),
-//     );
-
-//     // region:      --- Start Server
-//     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-//     let tcp = TcpListener::bind(&addr).await.unwrap();
-//     println!("->> LISTENING on {addr}\n");
-//     axum::serve(tcp, routes_hello).await.unwrap();
-//     // endregion:   --- Start Server
-// }
-
+#![allow(unused)]
 use axum::response::Html;
+use axum::response::IntoResponse;
 use axum::{Router, routing::get};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
@@ -27,11 +8,13 @@ use tokio::net::TcpListener;
 #[tokio::main]
 async fn main() {
     println!("Starting Rust web server...");
+    // let routes_hello = Router::new().route(
+    //     "/hello",
+    //     get(|| async { Html("Hello <strong>World.</strong>") }),
+    // );
 
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!</strong>") }),
-    );
+    //          ---- Start server
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("Attempting to bind to: {}", addr);
@@ -47,4 +30,13 @@ async fn main() {
             std::process::exit(1);
         }
     }
+
+    //          ---- Start server
+
+    //          ---- Handler Hello
+    async fn handler_hello() -> impl IntoResponse {
+        println!("->> {:<12} - handler_hello", "HANDLER");
+        Html("Hello <strong>World!</strong>")
+    }
+    //          ---- Handler Hello
 }
